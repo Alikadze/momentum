cd332e919b3cad9d43c6b59a548a4c7278afeb83
Alikadze <119809834+Alikadze@users.noreply.github.com>
2025-03-13 22:02:57 +0400

    add layout of project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'tasks',
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'tasks',
+                loadChildren: () => import('./features/task/task.routes').then(m => m.taskRoutes)
+            }
+        ]
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'tasks'
+    }
+];
---
+++ src/app/features/task/task.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TasksComponent } from './pages/tasks/tasks.component';
+
+export const taskRoutes: Routes = [
+    {
+        path: '',
+        component: TasksComponent,
+    }
+];
