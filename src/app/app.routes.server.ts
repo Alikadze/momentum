f64bae21c1256835949e0fd7a388917550258672
Alikadze <ralikadze@gmail.com>
2025-03-20 01:18:21 +0400

    hotfix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
+    {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+    },
+    {
+        path: 'tasks/:id',
+        renderMode: RenderMode.Client
+    }
 ];
